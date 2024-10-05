cfb2ebf8a456fba9faa2efeb3bf3ee23925237a5
DaiosHyperion <daioshyperion@gmail.com>
2024-10-05 01:18:05 -0400

    very important change

--- src/main/java/net/alcair/astralroots/AstralRoots.java
+++ src/main/java/net/alcair/astralroots/AstralRoots.java
@@ -1,17 +1,20 @@
 package net.alcair.astralroots;
 
+import net.alcair.astralroots.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class AstralRoots implements ModInitializer {
-	public static final String MOD_ID = "astral-roots";
+	public static final String MOD_ID = "astralroots";
 
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
+
+		ModItems.registerModItems();
 
 	}
 }
